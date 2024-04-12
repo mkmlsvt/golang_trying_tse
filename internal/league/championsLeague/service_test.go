5f81d7ff9a6d9474900ee252ca20e1748aa997c8
kemal.savut <kemal.savut@trendyol.com>
2024-04-12 18:02:11 +0300

    add unit tests

--- internal/league/championsLeague/service_test.go
+++ internal/league/championsLeague/service_test.go
@@ -7,16 +7,32 @@ import (
 
 func Test_ClChampions(t *testing.T) {
 	clService := NewChampionsLeagueService()
+	var result string
+	var err error
 	t.Run("Check if fair gt 5", func(t *testing.T) {
-		assert.Equal(t, "real madrid", clService.GetChampions(6))
-		assert.Equal(t, "real madrid", clService.GetChampions(6))
-		assert.Equal(t, "real madrid", clService.GetChampions(10))
+		result, err = clService.GetChampions(6)
+		assert.Equal(t, "real madrid", result)
+		assert.Nil(t, err)
+		result, err = clService.GetChampions(10)
+		assert.Equal(t, "real madrid", result)
+		assert.Nil(t, err)
 	})
 	t.Run("Check if fair lt 5", func(t *testing.T) {
-		assert.Equal(t, "kiev", clService.GetChampions(1))
-		assert.Equal(t, "kiev", clService.GetChampions(3))
+		result, err = clService.GetChampions(1)
+		assert.Equal(t, "kiev", result)
+		assert.Nil(t, err)
+		result, err = clService.GetChampions(3)
+		assert.Equal(t, "kiev", result)
+		assert.Nil(t, err)
 	})
 	t.Run("Check if fair equal 5", func(t *testing.T) {
-		assert.Equal(t, "barcelona", clService.GetChampions(5))
+		result, err = clService.GetChampions(5)
+		assert.Equal(t, "barcelona", result)
+		assert.Nil(t, err)
+	})
+	t.Run("Check if fair equal 0", func(t *testing.T) {
+		result, err = clService.GetChampions(0)
+		assert.Equal(t, "", result)
+		assert.NotNil(t, err)
 	})
 }
